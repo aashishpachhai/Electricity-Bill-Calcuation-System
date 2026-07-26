bc851544836f4c14aa5d2a87eac50742363a3f7d
Aashish Maharjan <aashishmaharjan700@gmail.com>
2026-07-26 19:50:30 +0545

    added edit renter function

--- apps/backend/src/controller/renter.controller.ts
+++ apps/backend/src/controller/renter.controller.ts
@@ -30,9 +30,34 @@ export const createRenter = async (req: Request, res: Response) => {
     message: "Renter inserted successfully",
   });
 };
-//   id: serial().primaryKey(),
-//   name: varchar().notNull(),
-//   room_number: integer().notNull(),
-//   email: varchar().notNull(),
-//   phone: varchar().notNull(),
-//   status: boolean().default(true),
+
+export const editRenter = async (req: Request, res: Response) => {
+  const { name, room_number, email, phone, status } = req.body;
+  const { id } = req.params;
+  if (!name || !room_number || !email || !phone || !id || !status) {
+    return res.status(400).json({
+      success: false,
+      message: "Some fields might be missing",
+    });
+  }
+  const data = await fromPromise(
+    db.update(renterTable).set({
+      email,
+      name,
+      phone,
+      room_number,
+      status,
+    }),
+    () => new Error("Database Error"),
+  );
+  if (data.isErr()) {
+    return res.status(500).json({
+      success: false,
+      message: data.error.message,
+    });
+  }
+  return res.status(200).json({
+    success: true,
+    message: "Renter updated successfully",
+  });
+};
